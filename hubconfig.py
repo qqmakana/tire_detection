30b76209d019a4e63ac9b0b68d8a8e9ff3b173fc
qqmakana <solarcouple@gmail.com>
2023-12-26 15:41:24 +0200

    Update hubconfig.py
    i have added few functions

--- hubconfig.py
+++ hubconfig.py
@@ -41,6 +41,56 @@ pretrained_vit_results = engine.train(model=pretrained_vit,
                                       device=device)
 
 
+# Get automatic transforms from pretrained ViT weights
+pretrained_vit_transforms = pretrained_vit_weights.transforms()
+print(pretrained_vit_transforms)
+
+
+import os
+
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+NUM_WORKERS = os.cpu_count()
+
+def create_dataloaders(
+    train_dir: str,
+    test_dir: str,
+    transform: transforms.Compose,
+    batch_size: int,
+    num_workers: int=NUM_WORKERS
+):
+
+  # Use ImageFolder to create dataset(s)
+  train_data = datasets.ImageFolder(train_dir, transform=transform)
+  test_data = datasets.ImageFolder(test_dir, transform=transform)
+
+  # Get class names
+  class_names = train_data.classes
+
+  # Turn images into data loaders
+  train_dataloader = DataLoader(
+      train_data,
+      batch_size=batch_size,
+      shuffle=True,
+      num_workers=num_workers,
+      pin_memory=True,
+  )
+  test_dataloader = DataLoader(
+      test_data,
+      batch_size=batch_size,
+      shuffle=False,
+      num_workers=num_workers,
+      pin_memory=True,
+  )
+
+  return train_dataloader, test_dataloader, class_names
+
+
+
+
+
+
 
 
 # Predict on custom image
