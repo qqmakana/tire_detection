926ec5b099dcb9b0eead0c8a47f12f530ebe3193
qqmakana <solarcouple@gmail.com>
2023-12-27 11:16:26 +0200

    Update hubconfig.py
    third try

--- hubconfig.py
+++ hubconfig.py
@@ -1,99 +1,17 @@
-import torch, 
-import torchvision, 
-import pytorch_pretrained_vit
-import os
+# hubconf.py
 import torch
-#import data_setup, engine, model_builder, utils
+import torchvision
+from timm.models.vision_transformer import vit_base_patch16_224 as _vit_base_patch16_224 # helper function
 
-# 1. Get pretrained weights for ViT-Base
-pretrained_vit_weights = torchvision.models.ViT_B_16_Weights.DEFAULT
+def PretrainedVit(pretrained=False, **kwargs):
+    """Pretrained Vision Transformer model with 16x16 patches and 224x224 input resolution
+    pretrained (bool): if True, load pretrained weights
+    kwargs: other arguments for the model
+    """
+    model = _vit_base_patch16_224(**kwargs) # create the model
+    if pretrained:
+        checkpoint = 'https://github.com/qqmakana/tire_detection/blob/main/Good.ipynb' # URL of the pretrained weights
+        state_dict = torch.hub.load_state_dict_from_url(checkpoint, progress=False) # load the state dict
+        model.load_state_dict(state_dict) # set the state dict
+    return model # return the model
 
-# 2. Setup a ViT model instance with pretrained weights
-pretrained_vit = torchvision.models.vit_b_16(weights=pretrained_vit_weights).to(device)
-
-# 3. Freeze the base parameters
-for parameter in pretrained_vit.parameters():
-    parameter.requires_grad = False
-
-# 4. Change the classifier head
-class_names = ['Bad_tire','Good_tire']
-
-set_seeds()
-pretrained_vit.heads = nn.Linear(in_features=768, out_features=len(class_names)).to(device)
-# pretrained_vit # uncomment for model output
-
-
-import engine
-
-# Create optimizer and loss function
-optimizer = torch.optim.Adam(params=pretrained_vit.parameters(),
-                             lr=1e-3)
-loss_fn = torch.nn.CrossEntropyLoss()
-
-# Train the classifier head of the pretrained ViT feature extractor model
-set_seeds()
-pretrained_vit_results = engine.train(model=pretrained_vit,
-                                      train_dataloader=train_dataloader_pretrained,
-                                      test_dataloader=test_dataloader_pretrained,
-                                      optimizer=optimizer,
-                                      loss_fn=loss_fn,
-                                      epochs=10,
-                                      device=device)
-
-
-# Get automatic transforms from pretrained ViT weights
-pretrained_vit_transforms = pretrained_vit_weights.transforms()
-print(pretrained_vit_transforms)
-
-
-import os
-
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
-
-NUM_WORKERS = os.cpu_count()
-
-def create_dataloaders(
-    train_dir: str,
-    test_dir: str,
-    transform: transforms.Compose,
-    batch_size: int,
-    num_workers: int=NUM_WORKERS
-):
-
-  # Use ImageFolder to create dataset(s)
-  train_data = datasets.ImageFolder(train_dir, transform=transform)
-  test_data = datasets.ImageFolder(test_dir, transform=transform)
-
-  # Get class names
-  class_names = train_data.classes
-
-  # Turn images into data loaders
-  train_dataloader = DataLoader(
-      train_data,
-      batch_size=batch_size,
-      shuffle=True,
-      num_workers=num_workers,
-      pin_memory=True,
-  )
-  test_dataloader = DataLoader(
-      test_data,
-      batch_size=batch_size,
-      shuffle=False,
-      num_workers=num_workers,
-      pin_memory=True,
-  )
-
-  return train_dataloader, test_dataloader, class_names
-
-
-
-
-
-
-
-
-# Predict on custom image
-pred_and_plot_image(model=pretrained_vit,
-                    image_path=custom_image_path,
-                    class_names=class_names)
