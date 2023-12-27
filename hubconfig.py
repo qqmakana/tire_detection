abb4dfbcea6839b0215b0a7004a404a0be7fc0c0
qqmakana <solarcouple@gmail.com>
2023-12-27 11:22:58 +0200

    Update hubconfig.py
    fourth editing

--- hubconfig.py
+++ hubconfig.py
@@ -3,7 +3,7 @@ import torch
 import torchvision
 from timm.models.vision_transformer import vit_base_patch16_224 as _vit_base_patch16_224 # helper function
 
-def PretrainedVit(pretrained=False, **kwargs):
+def Pretrained_vit(pretrained=False, **kwargs):
     """Pretrained Vision Transformer model with 16x16 patches and 224x224 input resolution
     pretrained (bool): if True, load pretrained weights
     kwargs: other arguments for the model
